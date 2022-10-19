7d26dab919995941d1f3243d101fdb504a9bc7e4
Sunith Christopher <sunith.christopher@gmail.com>
2022-10-19 15:28:49 +0100

    Modified app.py

--- Giraffe/app.py
+++ Giraffe/app.py
@@ -1,3 +1,10 @@
-print("Hello World")
-print("Sunith")
+character_name = "Tom"
+character_age = "50"
+print("There was once a man named " + character_name + ", ")
+print("He was " + character_age + " years old.")
+
+character_name = "Mike"
+print("He likes the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
+
 
