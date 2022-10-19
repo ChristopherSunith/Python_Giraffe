7a55aaa3ba6b63e4dc70a37f86dee31d40db2740
Sunith Christopher <sunith.christopher@gmail.com>
2022-10-19 15:48:57 +0100

    Modified app2.py

--- Giraffe/app2.py
+++ Giraffe/app2.py
@@ -1,7 +1,13 @@
 phrase = "Giraffe Academy"
+print(phrase.replace("Giraffe", "Elephant"))
 print(phrase.upper().isupper())
 print(phrase + " is cool.")
 print(len(phrase))
 print(phrase[0])
+print(phrase[3])
+print(phrase.index("a"))
+print(phrase.index("Acad"))
+print(phrase.index("z"))
+
 
 
